00be6f304cbc675f825275d142744133aa6f44ef
tadl <tadl@gmx.de>
2014-03-29 11:07:11 +0100

    studienbrief 4 abgabefertig

--- studienbrief4/uebung4_3/exif_manipulator.py
+++ studienbrief4/uebung4_3/exif_manipulator.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# python 2.x file due to pyexiv2 dependency
 #
 #  pdf_search.py
 #
@@ -6,14 +7,17 @@
 #
 #  This script provides a search in the text on a pdf file.
 #
-import urllib.request
+import urllib2 #python2.x
+from urlparse import urlsplit #python2.x
+from BeautifulSoup import BeautifulSoup #python2.x
+import pyexiv2 # python 2.x
+
 import argparse
-from urllib.parse import urlsplit
 from os.path import basename
-from bs4 import BeautifulSoup
 from PIL import Image
 from PIL.ExifTags import TAGS
 import os
+import datetime
 
 # exif manipulator class
 class ExifManipulator:
@@ -25,7 +29,7 @@ class ExifManipulator:
   # find image tags on a given url
   def findImages(self, url):
     print('[+] Finding images on ' + url)
-    urlContent= urllib.request.urlopen(url).read()
+    urlContent= urllib2.urlopen(url).read()
     soup = BeautifulSoup(urlContent)
     imgTags = soup.findAll('img')
     return imgTags
@@ -35,15 +39,15 @@ class ExifManipulator:
     try:
       print('[+] Dowloading image...')
       imgSrc = imgTag['src']
-      imgContent= urllib.request.urlopen(imgSrc).read()
+      imgContent= urllib2.urlopen(imgSrc).read()
       imgFileName = basename(urlsplit(imgSrc)[2])
-      print(imgFileName)
       imgFile = open(imgFileName, 'wb')
       imgFile.write(imgContent)
       imgFile.close()
+      print('Downloaded %s'%imgFileName)
       return imgFileName
     except Exception as err:
-      return 'Error: %s'%err
+      print('Error: %s'%err)
   
   # print exif data to stdout
   def showExifData(self, imgFileName):
@@ -59,13 +63,26 @@ class ExifManipulator:
       print('Error while extracting exif data from %s'%imgFileName)
   
   # manipulate a certain exif field
-  def modidyExifData(self, imgFileName, key, new_value):
+  def setExifTime(self, imgFileName, new_time):
     try:
-      imgFile = Image.open(imgFileName)
-      info = imgFile._getexif()
+      metadata = pyexiv2.ImageMetadata(imgFileName)
+      metadata.read()
       
-      # do change
-      # TODO
+      # find  date tags
+      date_tag1 = metadata['Exif.Image.DateTime']
+      date_tag2 = metadata['Exif.Photo.DateTimeOriginal']
+      date_tag3 = metadata['Exif.Photo.DateTimeDigitized']
+      
+      # modify data to current time
+      date_tag1.value = new_time
+      date_tag2.value = new_time
+      date_tag3.value = new_time
+
+      # write metadata to file
+      metadata.write()
+      metadata.read()
+      date_tag = metadata['Exif.Image.DateTime']
+      print("Saved value: %s"%date_tag.value)
          
     except Exception as err:
       print(err)
@@ -73,8 +90,12 @@ class ExifManipulator:
     
   # save an image without any exif data
   def removeAllExifData(self, imgFileName, output_file_name):
-    imgFile = Image.open(imgFileName)
-    imgFile.save(output_file_name) # ignores exif data
+    try:
+      imgFile = Image.open(imgFileName)
+      imgFile.save(output_file_name) # ignores exif data
+    except Exception as err:
+      print("Some error occured: %s"%err)
+      
       
 # main    
 def main():
@@ -87,12 +108,19 @@ def main():
   args = parser.parse_args()
   imgTags = em.findImages(args.url)
   
+  # for all found images
   for imgTag in imgTags:
     # download image to file
     imgFileName = em.downloadImage(imgTag)
 
+    # show exif data
     #em.showExifData(imgFileName)
-    #em.modidyExifData(imgFileName, 'date', 'today', imgFileName[:-4]+'_manipulaed_date.jpg')
+    
+    # set the exif date to the current time
+    current_time = datetime.datetime.today()
+    em.setExifTime(imgFileName, current_time)
+    
+    # store a copy of the image without any exif data
     #em.removeAllExifData(imgFileName, imgFileName[:-4]+'_without_exif.jpg')
       
 if __name__ == '__main__':
